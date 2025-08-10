d4936d4b7aa00bff59f1b6958ae1c13ad5b448ad
JoseRoberto <joserobertoapd@gmail.com>
2025-08-10 17:49:27 -0300

    add aula sobre switch

---
+++ src/academy/devdojo/maratonajava/Aula06Switch.java
@@ -0,0 +1,34 @@
+package academy.devdojo.maratonajava;
+
+public class Aula06Switch {
+    public static void main(String[] args) {
+        // imprima o dia da semana considerando 1 como domingo
+        int dia = 9 ;
+        switch (dia) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça-feira");
+                break;
+            case 4:
+                System.out.println("Quarta-feira");
+                break;
+            case 5:
+                System.out.println("Quinta-feira");
+                break;
+            case 6:
+                System.out.println("Sexta-feita");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+            default:
+                System.out.println("OPÇÃO INVALIDA");
+
+        }
+    }
+}
