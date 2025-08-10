924e060554beff8205b4db263ffbd064ad374b5e
JoseRoberto <joserobertoapd@gmail.com>
2025-08-09 21:11:51 -0300

    add aula sobre if e if-else part 02

--- src/academy/devdojo/maratonajava/Aula05EstruturasCondicionais01.java
+++ src/academy/devdojo/maratonajava/Aula05EstruturasCondicionais01.java
@@ -1,6 +1,6 @@
 package academy.devdojo.maratonajava;
 
-public class Aula05EstruturasCondicionais {
+public class Aula05EstruturasCondicionais01 {
     public static void main(String[] args){
        //Exemplo de utilização de if
         int idade = 18;
---
+++ src/academy/devdojo/maratonajava/Aula05EstruturasCondicionais02.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava;
+
+public class Aula05EstruturasCondicionais02 {
+    public static void main(String[] args) {
+        // usando else if
+        int idade = 13;
+        String categoria;
+
+        if(idade < 15){
+            categoria = "Categoria Infantil";
+        } else if (idade >= 15 && idade <18) {
+            categoria = "Categoria Juvenil";
+        }else {
+            categoria = "Categoria Adulta";
+        }
+        System.out.println(categoria);
+
+
+    }
+}
