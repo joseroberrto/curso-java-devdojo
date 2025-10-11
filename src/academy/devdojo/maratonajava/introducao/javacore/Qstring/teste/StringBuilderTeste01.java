727bdd6dc1ae22bb2a827af62c06e4aaa20cbe63
JoseRoberto <joserobertoapd@gmail.com>
2025-10-11 18:46:36 -0300

    adicionando aula sobre strings

---
+++ src/academy/devdojo/maratonajava/introducao/javacore/Qstring/teste/StringBuilderTeste01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.introducao.javacore.Qstring.teste;
+
+public class StringBuilderTeste01 {
+    public static void main(String[] args) {
+        String nome = "Willian";
+        nome = nome.concat(" suane");
+        System.out.println(nome);
+
+        System.out.println("--------------------------'");
+        StringBuilder sb = new StringBuilder(nome);
+        sb.append(" Suane").append(" Academy");
+        sb.reverse();
+        sb.reverse();//continua alterando o mesmo objeto
+        System.out.println(sb);
+
+
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/javacore/Qstring/teste/StringPerformaceTeste01.java
@@ -0,0 +1,51 @@
+package academy.devdojo.maratonajava.introducao.javacore.Qstring.teste;
+
+import java.awt.*;
+
+public class StringPerformaceTeste01 {
+    public static void main(String[] args) {
+        long inicio = System.currentTimeMillis();
+        concatStrings(1000);
+        long fim =System.currentTimeMillis();
+        System.out.println("Tempo gasto String:"+(fim-inicio)+"ms");
+        //1000 - 20ms
+
+        inicio = System.currentTimeMillis();
+        concatStringsBuilder(1000);
+        fim =System.currentTimeMillis();
+        System.out.println("Tempo gasto StringBuider:"+(fim-inicio)+"ms");
+        //1000 - 2ms
+
+
+        inicio = System.currentTimeMillis();
+        concatStringsBuffer(1000);
+        fim =System.currentTimeMillis();
+        System.out.println("Tempo gasto StringBuffer:"+(fim-inicio)+"ms");
+        //1000 - 3ms
+
+
+    }
+
+    private  static  void concatStrings(int tamanho){
+        String texto ="";
+        for (int i = 0; i < tamanho; i++) {
+            texto += i;
+
+        }
+    }
+
+    private  static  void concatStringsBuilder(int tamanho){
+        StringBuilder sb = new StringBuilder(tamanho);
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(i);
+
+        }
+    }
+    private  static  void concatStringsBuffer(int tamanho){
+        StringBuffer sb = new StringBuffer(tamanho);
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(i);
+
+        }
+    }
+}
