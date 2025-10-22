6e953f9308d5ba8bbd87546c480392f571eabf2a
JoseRoberto <joserobertoapd@gmail.com>
2025-10-22 18:09:15 -0300

    add aula sobre regex

---
+++ src/academy/devdojo/maratonajava/introducao/javacore/Vregex/PatternMatcherTest02.java
@@ -0,0 +1,32 @@
+package academy.devdojo.maratonajava.introducao.javacore.Vregex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest02 {
+    public static void main(String[] args){
+        /*
+        Meta Caracteres:
+        - \d = retorna todos os dígitos
+        - \D = retorna tudo que não for dígitos como letras,caracteres especias etc
+        - \s = retorna espaços em branco (\t \n \r)
+        - \S = retorna todos os caracteres excluindo o branco
+        - \w
+         */
+        String regex = "aba";
+        String texto = "abababa";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher =pattern.matcher(texto);
+
+        System.out.println("Texto: "+texto);
+        System.out.println("Indice:0123456789");
+        System.out.println("Regex: "+regex);
+        System.out.println("Posição encontrada: ");
+
+
+        while (matcher.find()){
+            System.out.print(matcher.start()+" "+matcher.group()+"\n");
+        }
+    }
+}
